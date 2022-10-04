309f89fb35a6348b62eba326af531aeba575c17c
Yalashanda <44616076+YalashandaA@users.noreply.github.com>
2022-10-04 11:41:24 +0200

    Manifest V3 fixes

--- js/redesign.js
+++ js/redesign.js
@@ -1,12 +1,12 @@
 chrome.storage.sync.get(['theme'], function(data) {
     if (data.theme != undefined && data.theme != "classic") {
         var link = document.createElement('link');
-        link.href = chrome.extension.getURL('css/designs/'+data.theme+'.css');
+        link.href = chrome.runtime.getURL('css/designs/'+data.theme+'.css');
         link.rel = "stylesheet";
         document.documentElement.appendChild(link);
         
         var link = document.createElement('link');
-        link.href = chrome.extension.getURL('css/post_redesign.css');
+        link.href = chrome.runtime.getURL('css/post_redesign.css');
         link.rel = "stylesheet";
         document.documentElement.appendChild(link);
     } 
