59b83ba3e833f161bfd2ee66c2861e06acc35e84
Loïc <36301183+ftLoic@users.noreply.github.com>
2022-12-21 11:05:59 +0100

    Useless variables

--- js/framesData.js
+++ js/framesData.js
@@ -5,8 +5,7 @@ https://github.com/KisaragiAyano/web-scripts
 Thanks to him for letting me use his scripts for Sakuga Extended!
 */
 
-var idHash, linkBooru, linkPost,
-    framerate = 24,
+var framerate = 24,
     frames = frame = 0,
     frames_html5 = 1/40,
     frames_arr = [],
@@ -16,13 +15,11 @@ var idHash, linkBooru, linkPost,
     booruLink = document.createElement('a'),
     gif, seek, control, text_frm, slider, play_gif, artistPartsConfirmed = 0, artistFrames;
 try {
-    idHash = /data\/([0-9a-z]+)/.exec(location.href)[1];
-    linkBooru = "https://www.sakugabooru.com/post.json?tags=md5%3A" + idHash;
-    fetch(linkBooru).then(res => {
+    var idHash = /data\/([0-9a-z]+)/.exec(location.href)[1];
+    fetch("https://www.sakugabooru.com/post.json?tags=md5%3A" + idHash).then(res => {
         res.json().then(posts => {
             if (posts && posts.length == 1) {
-                linkPost = "https://www.sakugabooru.com/post/show/" + posts[0].id;
-                booruLink.href = linkPost;
+                booruLink.href = "https://www.sakugabooru.com/post/show/" + posts[0].id;
             }
         });
     });
@@ -113,6 +110,7 @@ if (video || isGif) {
     //Link to the booru post
     var booruLinkText = document.createTextNode("Link to the post");
     booruLink.appendChild(booruLinkText); 
+    booruLink.classList.add('link');
     booruLink.id = 'booruLink';
     control.appendChild(booruLink); 
 
